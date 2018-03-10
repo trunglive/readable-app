0985e73756cfb58f239b6f84fe5318a81fc54d0d
Trung Vo <trungvo@Trungs-MacBook-Pro.local>
2018-03-09 23:11:12 -0500

    refactor(app): refactor components

--- src/components/CreatePostPage.js
+++ src/components/CreatePostPage.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import PostForm from './PostForm';
-import { fetchAddPost } from '../actions/postsActions';
+import React from "react";
+import { connect } from "react-redux";
+import PostForm from "./PostForm";
+import { fetchAddPost } from "../actions/postsActions";
 
 const CreatePostPage = props => (
   <div>
     <PostForm
       onSubmit={post => {
         props.dispatch(fetchAddPost(post));
-        props.history.push('/');
+        props.history.push("/");
       }}
     />
   </div>
